0ed807c1127f60bc96690cd5819afbd54a6d40db
vikitha28 <101237233+vikitha28@users.noreply.github.com>
2022-06-03 10:26:57 +0530

    Add files via upload

---
+++ microserviceTest/Pathology-Service-ms/Pathology-Service-ms/src/main/java/com/geetha/microservice/PathologyService/models/DiseasesList.java
@@ -0,0 +1,18 @@
+package com.geetha.microservice.PathologyService.models;
+
+import java.util.List;
+
+public class DiseasesList {
+
+	public List<Disease> diseases;
+
+	public List<Disease> getDiseases() {
+		return diseases;
+	}
+
+	public void setDiseases(List<Disease> diseases) {
+		this.diseases = diseases;
+	}
+	
+	
+}
---
+++ microserviceTest/Pathology-Service-ms/Pathology-Service-ms/src/test/java/com/geetha/Pathology/microservice/Pathology/Service/ms/PathologyServiceMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geetha.Pathology.microservice.Pathology.Service.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathologyServiceMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
